4cf75b80260a530fd6eff0934a8341cdb99815ca
Faith Oluwibe <faitholuwibe@gmail.com>
2022-06-29 07:55:17 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+import Button from "./components/Button";
 
 function App() {
+  const [counter, setCount] = useState(0);
+  let increasebutton = () => {
+    setCount(counter + 1);
+  }
+  let decreasebutton = () => {
+    setCount(counter - 1);
+  }
+  let resetbutton = () => {
+    setCount(0);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Counter</h1>
+        <h3>{counter}</h3>
+        <Button title={"decrease"} action={decreasebutton} />
+        <Button title={"increase"} action={increasebutton} />
+        <Button title={"reset"} action={resetbutton} />
+
       </header>
     </div>
-  );
+  )
+
 }
+
 
 export default App;
